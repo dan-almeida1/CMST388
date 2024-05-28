b944f0b8f651802816a87b1d0fd820ac79f68be1
Dan Almeida <daniella.almeida99@gmail.com>
2024-05-28 12:59:22 -0400

    variables

--- photo-gallery-assignment/js/image-gallery.js
+++ photo-gallery-assignment/js/image-gallery.js
@@ -1,1 +1,4 @@
 // JavaScript Document
+let largeImage = document.querySelector("#largeImage");
+let caption = document.querySelector("#caption");
+let thumbnails = document.querySelector("#thumbnails").children;
