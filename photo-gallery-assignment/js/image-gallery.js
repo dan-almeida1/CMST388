6ddcdda26fa32ef667614491f27467fad235639f
Dan Almeida <daniella.almeida99@gmail.com>
2024-05-28 13:05:27 -0400

    hover function

--- photo-gallery-assignment/js/image-gallery.js
+++ photo-gallery-assignment/js/image-gallery.js
@@ -2,3 +2,7 @@
 let largeImage = document.querySelector("#largeImage");
 let caption = document.querySelector("#caption");
 let thumbnails = document.querySelector("#thumbnails").children;
+
+function hoverImage(galleryImage) {
+    caption.innerHTML = galleryImage.alt;
+}
