13f5c70035c76b12af6beb2d2c32ac6ec6a7d09c
Dan Almeida <daniella.almeida99@gmail.com>
2024-07-01 18:29:43 -0400

    consts declared

--- form-validation-assignment/js/script.js
+++ form-validation-assignment/js/script.js
@@ -2,7 +2,12 @@ const form = document.getElementById('form');
 const firstName = document.getElementById('firstname');
 const lastName = document.getElementById('lastname');
 const address = document.getElementById('address');
-const errorElement = document.getElementById('error')
+const city = document.getElementById('city');
+const state = document.getElementById('state');
+const zipcode = document.getElementById('zipcode');
+const phone = document.getElementById('phone');
+const email = documents.getElementById('email');
+const errorElement = document.getElementById('error');
 //error sign
 form.addEventListener('submit', (e) => {
     let messages = []
